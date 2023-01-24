eae15dbd247d062d4e35279b7460b62ac2969db2
joseConejoMadureira <jose.games15@outlook.com>
2023-01-24 13:03:55 -0300

    inputTask

--- src/components/InputTask.vue
+++ src/components/InputTask.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-    <input class="new-todo" placeholder="O que precisa ser feito?">
+    <input class="new-todo" v-on:keyup.enter="addTask" placeholder="O que precisa ser feito?" />
   </div>
 </template>
 
 <script>
+import { Task } from '../models/Task'
 export default {
-
+  methods: {
+    addTask ($event) {
+      let task = new Task()
+      let value = $event.target.value
+      task.completed = false
+      task.title = value
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
