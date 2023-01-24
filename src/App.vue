bea4ceb93fcfe3f038baeba098b6baeb094a41ce
joseConejoMadureira <jose.games15@outlook.com>
2023-01-24 13:26:52 -0300

    list

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,26 @@
 
 <script>
 import InputTask from './components/InputTask.vue'
+import { Task } from './models/Task'
+let tasks = []
+let task = new Task()
+task.completed = false
+task.title = 'tarefa'
+tasks.push(task)
+tasks.push(task)
+tasks.push(task)
 
 export default {
   name: 'App',
-  components: {InputTask}
+  components: {InputTask},
+  data () {
+    return {
+      tasks: tasks
+    }
+  }
 }
+
 </script>
 
 <style>
-
 </style>
