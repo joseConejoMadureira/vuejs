cd9ead2b0b7ba159f45e2e8f2e954fa43d5680f1
joseConejoMadureira <jose.games15@outlook.com>
2023-01-25 16:25:12 -0300

    router

--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import routes from './routes'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
+  router,
   el: '#app',
   components: { App },
   template: '<App/>'
