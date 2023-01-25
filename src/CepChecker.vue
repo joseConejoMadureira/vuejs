dd92f752961c94af58b12c72474c495580d3a1f3
joseConejoMadureira <jose.games15@outlook.com>
2023-01-25 16:34:00 -0300

    routes

---
+++ src/CepChecker.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="cepChecker">
+    <label>Digite seu cep</label>
+    <input type="text">
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
